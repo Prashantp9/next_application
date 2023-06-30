9975acb49d808583fbb7af41c3eb383489052c77
Prashantp9 <prashantdsk75@gmail.com>
2023-06-30 14:02:43 +0530

    product card prepared

---
+++ source/client/src/app/[categories]/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Categories() {
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+  const getFakeCategories = async (): Promise<void> => {
+    try {
+      const response = await fetch(
+        "https://fakestoreapi.com/products/categories"
+      );
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getFakeData = async (): Promise<void> => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getFakeData();
+    getFakeCategories();
+  }, []);
+  return (
+    <div className="p-3">
+      <h4 className="mb-2 mt-0 text-3xl font-medium leading-tight text-primary text-gray-400 mb-5">
+        Categories
+      </h4>
+      <div className="flex flex-wrap content-around w-full gap-2 border-b-4  border-slate-400 pb-7">
+        {categories.concat()?.map((elm: any, idx: number) => (
+          <div className="p-5 shadow-lg rounded-md hover:bg-neutral-800 bg-neutral-900">
+            <p className="text-gray-400 font-semibold">{elm}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className="grid grid-cols-5 gap-4 w-full p-3 overflow-y-scroll">
+        {products.map((elm: any, idx: number) => (
+          <div className="flex flex-col p-2 bg-stone-800 rounded-md cursor-pointer">
+            <div className="w-full h-48 mb-3">
+              <img
+                className="object-cover h-full w-full"
+                src={elm?.image}
+                alt=""
+              />
+            </div>
+            <p className="text-gray-200 text-sm font-semibold">
+              {elm?.title?.slice(0, 15) + "..."}
+            </p>
+            <div className="h-16">
+              <p className="text-secondary-700 text-xs">
+                {elm?.description?.slice(0, 100)}
+              </p>
+            </div>
+            <p className="text-gray-200 text-base font-semibold">
+              {"$" + elm?.price}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
