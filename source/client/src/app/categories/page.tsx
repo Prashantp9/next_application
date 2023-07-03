be15803a96dcfd88d7d6c8fd11c3849ec6aed5c5
Prashantp9 <prashantdsk75@gmail.com>
2023-07-03 18:15:35 +0530

    svg added

--- source/client/src/app/categories/page.tsx
+++ source/client/src/app/categories/page.tsx
@@ -117,9 +117,29 @@ export default function Categories() {
                 {elm?.description?.slice(0, 100)}
               </p>
             </div>
-            <p className="text-gray-200 text-base font-semibold">
-              {"$" + elm?.price}
-            </p>
+            <div className="flex justify-between w-full h-10 align-center">
+              <p className="text-gray-200 text-base font-semibold align-center">
+                {"$" + elm?.price}
+              </p>
+              <div className="flex gap-2">
+                <button className="text-xs font-semibold">
+                  <svg
+                    className="w-7"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M2 3H4.5L6.5 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM6.07142 14H18L21 5H4.78571M11 19C11 20.1046 10.1046 21 9 21C7.89543 21 7 20.1046 7 19C7 17.8954 7.89543 17 9 17C10.1046 17 11 17.8954 11 19Z"
+                      stroke="#000000"
+                      stroke-width="1.5"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                </button>
+                <button className="text-xs font-semibold"></button>
+              </div>
+            </div>
           </div>
         ))}
       </div>
