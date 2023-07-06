efccf6dc7ed4c1635158e1a4ab9a644bfd4295f9
Prashantp9 <prashantdsk75@gmail.com>
2023-07-06 09:26:32 +0530

    swiper js imported

--- source/client/src/app/page.tsx
+++ source/client/src/app/page.tsx
@@ -1,16 +1,36 @@
 "use client";
 
 import "./styles/home.css";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/free-mode";
+import "swiper/css/navigation";
+import "swiper/css/thumbs";
 
 import { AppDispatch, useAppSelector } from "./redux/store";
+import {
+  Autoplay,
+  FreeMode,
+  Lazy,
+  Navigation,
+  Pagination,
+  Thumbs,
+  Zoom,
+} from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { cookieTest, test } from "./redux/features/userAuthSlice";
+import { useEffect, useState } from "react";
 
 import Login from "./login";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
 
 export default function Home() {
+  const [thumbsSwiper, setThumbsSwiper] = useState(null);
   return (
-    <></>
+    <>
+      <div className="flex justify-center w-full h-52">
+        <div className="flex w-3/4 h-full"></div>
+      </div>
+    </>
   );
 }
