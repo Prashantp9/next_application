bedafdb467746ff10db917652e7b4bde24cceda9
Prashantp9 <prashantdsk75@gmail.com>
2023-07-22 12:07:34 +0530

    categories drop down has been added to the group

--- source/client/src/app/[categories]/page.tsx
+++ source/client/src/app/[categories]/page.tsx
@@ -13,8 +13,7 @@ import { useSearchParams } from "next/navigation";
 export default function Categories() {
   // useState states
   const [categories, setCategories] = useState([]);
-  const [products, setProducts] = useState([]);
-  const [mProducts, setMProducts] = useState([]);
+  const [isCategory, setCategory] = useState(false);
   // Hooks
   const router = useRouter();
   const pathname = usePathname();
@@ -41,18 +40,6 @@ export default function Categories() {
     }
   };
 
-  const getFakeData = async (): Promise<void> => {
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      const data = await response.json();
-      setMProducts(data);
-      setProducts(data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  //============================================
-  //===========================================Query fuction
   const createQueryString = useCallback(
     (name: string, value: string) => {
       const params = new URLSearchParams(searchParams.toString());
@@ -69,7 +56,6 @@ export default function Categories() {
   //   =======================================
 
   useEffect(() => {
-    getFakeData();
     getFakeCategories();
     dispatch(fetchProductData({}));
   }, []);
@@ -81,44 +67,55 @@ export default function Categories() {
   }, [category]);
 
   return (
-    <div className="p-3">
-      <h4 className="mb-2 mt-0 font-medium leading-tight text-xl text-gray-400 ">
-        filter
-      </h4>
-
-      <div className="flex flex-wrap content-around w-full gap-2 border-b-4  border-slate-400 pb-7">
-        <div className="flex flex-col  gap-2">
-          <label
-            htmlFor="categories"
-            className="text-base font-semibold text-slate-300 pl-1"
+    <div className="py-4">
+      <div className="w-[2rem] px-5">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height={24}
+          // fill="rgb(148 163 184)"
+          fill="#dfdf"
+          id="filter"
+        >
+          <path
+            d="M20 5h-1.17a3.001 3.001 0 0 0-5.66 0H4a1 1 0 0 0 0 2h9.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2zm-4 2a1 1 0 1 0 0-2 1 1 0 0 0 0 2zM3 12a1 1 0 0 1 1-1h1.17a3.001 3.001 0 0 1 5.66 0H20a1 1 0 1 1 0 2h-9.17a3.001 3.001 0 0 1-5.66 0H4a1 1 0 0 1-1-1zm5 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm-4 4a1 1 0 1 0 0 2h9.17a3.001 3.001 0 0 0 5.66 0H20a1 1 0 1 0 0-2h-1.17a3.001 3.001 0 0 0-5.66 0H4zm13 1a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </div>
+      <div className="flex flex-wrap content-around w-full gap-2 border-b-4  border-slate-400 py-4 px-5">
+        <div
+          onClick={() => setCategory(!isCategory)}
+          className="flex text-sm  lg:text-base justify-between items-center px-8 gap-10 py-2 relative border-2 border-slate-400 rounded-lg cursor-pointer "
+        >
+          Categories
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            id="dropdown"
+            x="0"
+            y="0"
+            version="1.1"
+            viewBox="0 0 29 29"
+            width={20}
+            fill="rgb(148 163 184)"
+            className="cursor-pointer"
           >
-            Categories
-          </label>
-          <select
-            name="categories"
-            id="categories"
-            className="px-4  rounded-md bg-transparent border-2 border-slate-600 focus:bg-neutral-900 cursor-pointer py-2"
-          >
-            {categories?.map((elm: any, idx: number) => (
-              <option
-                className="text-base  bg-neutral-900 cursor-pointer"
-                value={elm}
-              >
-                {elm}
-              </option>
-            ))}
-          </select>
+            <path d="M14.5 27.065a12.465 12.465 0 0 1-8.839-3.655c-4.874-4.874-4.874-12.804 0-17.678 2.361-2.361 5.5-3.662 8.839-3.662s6.478 1.3 8.839 3.662c4.874 4.874 4.874 12.804 0 17.678a12.465 12.465 0 0 1-8.839 3.655zm0-22.995a10.43 10.43 0 0 0-7.425 3.076c-4.093 4.094-4.093 10.755 0 14.85 4.094 4.093 10.755 4.093 14.85 0 4.093-4.094 4.093-10.755 0-14.85A10.434 10.434 0 0 0 14.5 4.07zm8.132 18.633h.01-.01z"></path>
+            <path d="M14.5 17.869a.997.997 0 0 1-.707-.293L9.197 12.98a.999.999 0 1 1 1.414-1.414l3.889 3.889 3.889-3.889a.999.999 0 1 1 1.414 1.414l-4.596 4.596a.997.997 0 0 1-.707.293z"></path>
+          </svg>
+          {isCategory && (
+            <div className="flex flex-col h-[15rem] overflow-y-scroll top-10 left-0 right-0 rounded-md absolute z-10 bg-neutral-700 py-2 ">
+              {categories.concat(categories).map((elm: any, idx: Number) => (
+                <p
+                  onClick={() => changeSearchParams("category", elm)}
+                  className=" pl-[1rem] pr-[1rem] py-3 text-slate-100  whitespace-nowrap"
+                >
+                  {elm}
+                </p>
+              ))}
+            </div>
+          )}
         </div>
-        {/* {categories?.map((elm: any, idx: number) => (
-          <div
-            onClick={() => changeSearchParams("category", elm)}
-            className={`cursor-pointer p-5 shadow-lg rounded-md hover:bg-neutral-800  ${
-              category && category == elm ? "bg-neutral-800" : "bg-neutral-900"
-            }`}
-          >
-            <p className="text-gray-400 font-semibold">{elm}</p>
-          </div>
-        ))} */}
       </div>
       <div className="flex justify-center w-full overflow-y-scroll h-[80vh]">
         <div className="grid w-[90%]  justify-items-center grid-cols-1 pt-4 h-full gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
